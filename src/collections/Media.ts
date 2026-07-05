153dbde313aa0c07063ed9a9b33dc442e60f6cce
Ken Escaño <kenescano@Kens-MacBook-Air.local>
2026-07-05 11:49:32 +0800

    fix

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -7,6 +7,7 @@ export const Media: CollectionConfig = {
   },
   upload: {
     staticDir: 'media',
+    disableLocalStorage: true,
     imageSizes: [
       { name: 'thumbnail', width: 400, height: 300, position: 'centre' },
       { name: 'card', width: 800, height: 600, position: 'centre' },
