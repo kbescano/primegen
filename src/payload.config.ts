6df41998554057ef9740dbc70853d2ddc4a2ead3
Ken Escaño <kenescano@Kens-MacBook-Air.local>
2026-07-21 22:43:19 +0800

    fix

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -26,6 +26,14 @@ const dirname = path.dirname(filename)
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000',
+  cors: [
+    process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000',
+    'http://localhost:3000',
+  ],
+  csrf: [
+    process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000',
+    'http://localhost:3000',
+  ],
   admin: {
     user: Users.slug,
     meta: {
